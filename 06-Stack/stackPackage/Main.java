6c82a30d0cababf4125de02f65bb59766f520282
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-07-11 23:00:14 +0530

    implementing Stack using Arrays

---
+++ 06-Stack/stackPackage/Main.java
@@ -0,0 +1,28 @@
+package stackPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack newStack = new Stack(5);
+        boolean isEmpty = newStack.isEmpty();
+        boolean isFull = newStack.isFull();
+        System.out.println(isEmpty);
+        System.out.println(isFull);
+        newStack.push(5);
+        newStack.push(10);
+        newStack.push(16);
+        newStack.push(20);
+        newStack.push(25);
+        int pop = newStack.pop();
+        System.out.println("Popped item is : " + pop);
+        int pop1 = newStack.pop();
+        System.out.println("Popped item is : " + pop1);
+        int peek = newStack.peek();
+        System.out.println("peeked item is :" + peek);
+        int peek1 = newStack.peek();
+        System.out.println("peeked item is :" + peek1);
+        newStack.deleteStack();
+        int peek2 = newStack.peek();
+        System.out.println("peeked item is :" + peek2);
+
+    }
+}
---
+++ 06-Stack/stackPackage/Stack.java
@@ -0,0 +1,71 @@
+package stackPackage;
+
+public class Stack {
+    int array[];
+    int topOfStack;
+
+    public Stack(int size) {
+        this.array = new int[size];
+        this.topOfStack = -1;
+        System.err.println("The Stack is Creted with size of " + size);
+    }
+
+    // isEmpty Method
+    public boolean isEmpty() {
+        if (topOfStack == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // isFull Method
+    public boolean isFull() {
+        if (topOfStack == array.length - 1) {
+            // System.out.println("Stack is Full");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // push Method
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is Full");
+        } else {
+            array[++topOfStack] = value;
+            System.out.println(value + " is Inserted at index : " + topOfStack);
+
+        }
+    }
+
+    // pop method
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            int topStack = array[topOfStack];
+            topOfStack--;
+            return topStack;
+        }
+    }
+
+    // peek Method
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            return array[topOfStack];
+        }
+    }
+
+    // delete method
+    public void deleteStack() {
+        array = null;
+        topOfStack = -1;
+        System.out.println("Stack deleted Successfully");
+    }
+}
