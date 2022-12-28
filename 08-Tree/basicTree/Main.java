194cdeadb4b917a38332370cf52bd56bf1c339a7
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-12-28 22:15:28 +0530

    Basic Tree Creation

---
+++ 08-Tree/basicTree/Main.java
@@ -0,0 +1,27 @@
+package basicTree;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode drinks = new TreeNode("Drinks");
+
+        TreeNode hot = new TreeNode("Hot");
+        TreeNode cold = new TreeNode("Cold");
+
+        TreeNode tea = new TreeNode("Tea");
+        TreeNode coffee = new TreeNode("Coffee");
+        TreeNode wine = new TreeNode("Wine");
+        TreeNode beer = new TreeNode("Beer");
+
+        drinks.addChild(hot);
+        drinks.addChild(cold);
+
+        hot.addChild(tea);
+        hot.addChild(coffee);
+
+        cold.addChild(beer);
+        cold.addChild(wine);
+
+        System.out.println(drinks.print(0));
+
+    }
+}
